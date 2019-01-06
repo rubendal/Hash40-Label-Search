000cd9469cae9776f4d6f03075542b387092fd20
Ruben <rubendalssb@gmail.com>
2019-01-06 13:21:41 -0600

    Improve dictionary search

--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,7 @@ dictionary = []
 prefix = ''
 suffix = ''
 length = 0
+depth = 0
 checkAll = False
 hashes = []
 defaultPath = "hashes.txt"
@@ -35,20 +36,28 @@ def check(string):
     if hash in hashes:
         print("{0} -> {1} length: {2}".format(hash, prefix + string + suffix, len(prefix + string + suffix)))
 
-def process(prevWords, prevWord, string):
+def checkLoop(currentDepth, prevWords, prevWord, string):
+    global depth
+    if(depth == 0):
+        loop(currentDepth, prevWords, prevWord, string)
+    else:
+        if(currentDepth < depth):
+            loop(currentDepth, prevWords, prevWord, string)
+
+def process(currentDepth, prevWords, prevWord, string):
     global length, checkAll
     if(checkAll):
         if(len(string) + getInputLength() < length):
             check(string)
-            loop(prevWords, prevWord, string)
+            checkLoop(currentDepth + 1, prevWords, prevWord, string)
     else:
         if(len(string) + getInputLength() == length):
             check(string)
         else:
             if(len(string)  + getInputLength() < length):
-                loop(prevWords, prevWord, string)
+                checkLoop(currentDepth + 1, prevWords, prevWord, string)
 
-def loop(prevWords, prevWord, string):
+def loop(currentDepth, prevWords, prevWord, string):
     global dictionary
     for word in dictionary:
         if(word not in prevWords):
@@ -56,27 +65,28 @@ def loop(prevWords, prevWord, string):
                 words = prevWords.copy()
                 if(word != "_"):
                     words.append(word)
-                process(words, word, string + "_" + word)
+                process(currentDepth, words, word, string + "_" + word)
 
 #Prints first character to show progress
 def StartLoop():
     global dictionary
     for word in dictionary:
         print(word)
-        process([word], word, word)
+        process(0, [word], word, word)
 
 def start(argv):
-    global defaultPath, prefix, suffix, length, checkAll
+    global defaultPath, prefix, suffix, length, checkAll, depth
     path = defaultPath
     try:
-      opts, args = getopt.getopt(argv,"hl:p:s:f",["suffix=","prefix=","length=", "checkAll", "file="])
+      opts, args = getopt.getopt(argv,"hl:d:p:s:f",["suffix=","prefix=","length=", "checkAll", "file=", "depth="])
     except getopt.GetoptError:
-        print('dictionary.py -l <int> (-p <string>) (-s <string>) (--checkAll)')
+        print('dictionary.py -l <int> (-d <int>) (-p <string>) (-s <string>) (--checkAll)')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print('dictionary.py -l <int> (-p <string>) (-s <string>) (--checkAll)')
             print("-l/--length= <int>: Max length of strings, takes into account prefix and suffix. If checkAll isn't enabled only strings with this length will be hashed and compared with list")
+            print("-d/--depth= <int>: Max amount of words to use (prefix/suffix not counted)")
             print("-p/--prefix= <string>: Prefix to add to generated strings")
             print("-s/--suffix= <string>: Suffix to add to generated strings")
             print("--checkAll: Check all strings hashes regardless of length")
@@ -87,6 +97,8 @@ def start(argv):
             suffix = arg
         elif opt in ("--length", "-l"):
             length = int(arg)
+        elif opt in ("--depth", "-d"):
+            depth = int(arg)
         elif opt in ("--checkAll"):
             checkAll = True
         elif opt in ("--file", "-f"):
